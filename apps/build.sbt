916b53efbb6de281de473e4f2529eb6bd40c3cab
owfdataboy <phanvigiaii@gmail.com>
2022-04-22 10:03:50 +0700

    push new spark-kafka job

--- apps/build.sbt
+++ apps/build.sbt
@@ -1,10 +1,23 @@
-name := "Kafka Streaming Project"
+ThisBuild / version := "0.2.0"
 
-version := "1.0"
+ThisBuild / scalaVersion := "2.12.10"
 
-scalaVersion := "2.12.10"
+lazy val root = (project in file("."))
+  .settings(
+    name := "Kafka Streaming Project"
+  )
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "3.0.3"
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "3.0.2"
-libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.2.1"
-libraryDependencies += "org.apache.kafka" % "kafka-clients" % "0.8.2.0"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.0.3",
+  "org.apache.spark" %% "spark-streaming" % "3.0.2" % "provided",
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.0.2",
+  "org.apache.spark" %% "spark-sql" % "3.0.3",
+  "org.apache.kafka" % "kafka-clients" % "3.1.0",
+  "org.apache.kafka" %% "kafka" % "3.1.0",
+  "com.github.nscala-time" %% "nscala-time" % "2.30.0"
+)
+
+assembly / assemblyMergeStrategy := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
--- apps/src/main/scala/kafka.scala
+++ apps/src/main/scala/kafka.scala
@@ -1,49 +1,41 @@
+import java.util.Properties
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.Seconds
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.kafka010.KafkaUtils
-import org.apache.spark.streaming.kafka010.LocationStrategies
-import org.apache.spark.streaming.kafka010.ConsumerStrategies
-import org.apache.spark.streaming.kafka010.ConsumerStrategies
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.spark.streaming.kafka010.PreferConsistent
 import org.apache.spark.streaming.kafka010.LocationStrategies
 import org.apache.spark.streaming.kafka010.ConsumerStrategies
-import org.apache.spark.streaming.kafka010.LocationStrategies
-import org.apache.spark.streaming.kafka010.ConsumerStrategies
-import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.producer.KafkaProducer
-import java.util.Properties
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.spark.streaming.Seconds
-import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
 
-object KafkaStreaming {
-  def main(args:Array[String]){
+object KafkaApp {
+  def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("SparkKafkaStreaming").setMaster("local[*]")
     val ssc = new StreamingContext(conf,Seconds(15))
     val sc = ssc.sparkContext
     sc.setLogLevel("ERROR")
-    val kafkaParams = giveMeKafkaProps(Array("localhost:9092","console-consumer-group"))
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("testtopic")
     val stream = KafkaUtils.createDirectStream[String, String](
       ssc,
       LocationStrategies.PreferConsistent,
-      ConsumerStrategies.Subscribe[String, String](Set("testtopic"), kafkaParams))
+      ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
+    )
     stream.print()
     ssc.start()
     ssc.awaitTermination()
- }
-
- def giveMeKafkaProps(params:Array[String]) : Map[String, Object] ={
-    val kafkaParams = Map[String, Object](
-        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> params(0),
-        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-        ConsumerConfig.GROUP_ID_CONFIG -> params(1),
-    )
-    return kafkaParams
   }
 }
